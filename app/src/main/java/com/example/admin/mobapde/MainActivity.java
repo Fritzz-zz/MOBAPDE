20b144404d27177df1d3c2e0b6bb0281d037cabc
ssprite28 <miguelrivera122@gmail.com>
2018-12-01 15:47:16 +0800

    initial push

--- app/src/main/java/com/example/admin/mobapde/MainActivity.java
+++ app/src/main/java/com/example/admin/mobapde/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.admin.mobapde;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recycler = findViewById(R.id.recycler);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //(2) Inflaters are used to populate items from the layout XML. These can be used to make
+        //    views without needing to arrange the items programaitcally.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_layout, menu);
+        return true;
     }
 }
