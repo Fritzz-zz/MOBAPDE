1d4a1add62777f73b3de11e261a14b6763db6586
ssprite28 <miguelrivera122@gmail.com>
2018-12-06 16:45:45 +0800

    Added options menu with listeners

--- app/src/main/java/com/example/admin/mobapde/Fragments/OptionsFragment.java
+++ app/src/main/java/com/example/admin/mobapde/Fragments/OptionsFragment.java
@@ -3,21 +3,31 @@ package com.example.admin.mobapde.Fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.admin.mobapde.BrowseRecycler.BrowseAdapter;
 import com.example.admin.mobapde.R;
 
 public class OptionsFragment extends Fragment {
 
-    private RecyclerView recycler;
-    private BrowseAdapter adapter;
-    private RecyclerView.LayoutManager manager;
+    private ImageView op1;
+    private ImageView op2;
+    private ImageView op3;
+    private ImageView op4;
+
+    private ConstraintLayout option1;
+    private ConstraintLayout option2;
+    private ConstraintLayout option3;
+    private ConstraintLayout option4;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,7 +38,7 @@ public class OptionsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        final View view = inflater.inflate(R.layout.fragment_browse, container, false);
+        final View view = inflater.inflate(R.layout.fragment_options, container, false);
         return view;
     }
 
@@ -36,11 +46,51 @@ public class OptionsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        recycler = view.findViewById(R.id.recycler);
-        manager = new LinearLayoutManager(getContext());
-        recycler.setLayoutManager(manager);
+        op1 = view.findViewById(R.id.optionImg1);
+        op2 = view.findViewById(R.id.optionImg2);
+        op3 = view.findViewById(R.id.optionImg3);
+        op4 = view.findViewById(R.id.optionImg4);
 
-        adapter = new BrowseAdapter();
-        recycler.setAdapter(adapter);
+        option1 = view.findViewById(R.id.option1);
+        option2 = view.findViewById(R.id.option2);
+        option3 = view.findViewById(R.id.option3);
+        option4 = view.findViewById(R.id.option4);
+
+        op1.setImageResource(R.drawable.ic_edit_account);
+        op2.setImageResource(R.drawable.ic_credit_card);
+        op3.setImageResource(R.drawable.ic_attach_money);
+        op4.setImageResource(R.drawable.ic_cance);
+
+        option1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "TEST", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        option2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "TEST", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        option3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "TEST", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        option4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "TEST", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
     }
 }
