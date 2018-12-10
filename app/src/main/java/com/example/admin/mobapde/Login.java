9686ab622becd903a1eb51c173dd112c46ca18f1
Unknown <kyle_sim@dlsu.edu.ph>
2018-12-10 18:24:18 +0800

    Navigation for Login and Signup
    Added the ability to navigate through the different activities

--- app/src/main/java/com/example/admin/mobapde/Login.java
+++ app/src/main/java/com/example/admin/mobapde/Login.java
@@ -1,15 +1,42 @@
 package com.example.admin.mobapde;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity{
+
+    private TextInputEditText email, pass;
+    private Button login, signup;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
 
+        email = findViewById(R.id.textMailL);
+        signup = findViewById(R.id.SignUpLbtn);
+        login = findViewById(R.id.loginLbtn);
+        pass = findViewById(R.id.textPassL);
 
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sign = new Intent(getApplicationContext(), SignUp.class);
+                Login.this.startActivity(sign);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/admin/mobapde/SignUp.java
+++ app/src/main/java/com/example/admin/mobapde/SignUp.java
@@ -1,16 +1,44 @@
 package com.example.admin.mobapde;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
 
+    private TextInputEditText email, pass, cpass;
+    private Button login, signup;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_page);
 
+        email = findViewById(R.id.textMailS);
+        pass = findViewById(R.id.textPassS);
+        cpass = findViewById(R.id.textCPassS);
+        login = findViewById(R.id.loginSbtn);
+        signup = findViewById(R.id.SignUpSbtn);
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent log = new Intent(getApplicationContext(), Login.class);
+                SignUp.this.startActivity(log);
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //add to firebase
+            }
+        });
 
     }
+
+
 }
