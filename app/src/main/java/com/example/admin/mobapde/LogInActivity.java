bce1e3ddd3d7132537a61b8aeba05bff49792ac7
Fritzz-zz <Fritz_calimag@dlsu.edu.ph>
2018-12-10 20:02:23 +0800

    log in & sign up

--- app/src/main/java/com/example/admin/mobapde/LogInActivity.java
+++ app/src/main/java/com/example/admin/mobapde/LogInActivity.java
@@ -1,17 +1,25 @@
 package com.example.admin.mobapde;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class LogInActivity extends AppCompatActivity{
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LogInActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Context c;
 
@@ -24,8 +32,9 @@ public class LogInActivity extends AppCompatActivity{
 
     private ImageView img;
 
+    private FirebaseAuth mAuth;
 
-
+    private String Email,Password;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,6 +42,8 @@ public class LogInActivity extends AppCompatActivity{
         setContentView(R.layout.activity_log_in);
         this.c = this;
 
+        findViewById(R.id.RegTxt).setOnClickListener(this);
+        findViewById(R.id.loginProceedBtn).setOnClickListener(this);
         login = findViewById(R.id.logInText);
         password = findViewById(R.id.logInPassword);
         toolbar = findViewById(R.id.toolbar);
@@ -41,6 +52,7 @@ public class LogInActivity extends AppCompatActivity{
 
         img.setImageResource(R.drawable.ic_person);
 
+        mAuth = FirebaseAuth.getInstance();
 
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
@@ -52,7 +64,7 @@ public class LogInActivity extends AppCompatActivity{
             }
         });
 
-        proceed.setOnClickListener(new View.OnClickListener() {
+        /*proceed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -64,11 +76,72 @@ public class LogInActivity extends AppCompatActivity{
 
 
             }
+        });*/
+
+
+
+
+
+    }
+
+    private void userLogin(){
+        Email = login.getText().toString().trim();
+        Password = password.getText().toString().trim();
+
+        if(Email.isEmpty()){
+            login.setError("Email is Required");
+            login.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
+            login.setError("Please enter a valid Email");
+            login.requestFocus();
+            return;
+
+        }
+
+        if(Password.isEmpty()){
+            password.setError("Password is required");
+            password.requestFocus();
+            return;
+        }
+
+        if (Password.length()<6){
+            password.setError("Minimum length of Password should be 6 characters");
+            password.requestFocus();
+            return;
+        }
+
+
+        mAuth.signInWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "Log In Successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.RegTxt:
 
+                startActivity(new Intent(this, SignUpActivity.class));
 
+                break;
 
+            case R.id.loginProceedBtn:
+                userLogin();
+                break;
 
+        }
     }
 }
--- app/src/main/java/com/example/admin/mobapde/SignUpActivity.java
+++ app/src/main/java/com/example/admin/mobapde/SignUpActivity.java
@@ -1,26 +1,38 @@
 package com.example.admin.mobapde;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
-public class SignUpActivity extends AppCompatActivity {
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Context c;
 
-    private EditText email;
-    private EditText password;
+    private EditText EmailRegTxt;
+    private EditText PassRegTxt;
     private ImageView img;
     private Button btn;
     private Toolbar toolbar;
+    private String Email, Password;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,10 +40,12 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
         this.c = this;
 
-        email = findViewById(R.id.signupEditEmail);
-        password = findViewById(R.id.signupEditPass);
+        EmailRegTxt= findViewById(R.id.signupEditEmail);
+        PassRegTxt = findViewById(R.id.signupEditPass);
         img = findViewById(R.id.signupImg);
         btn = findViewById(R.id.signupProceed);
+
+        mAuth = FirebaseAuth.getInstance();
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -46,7 +60,9 @@ public class SignUpActivity extends AppCompatActivity {
 
         img.setImageResource(R.drawable.ic_person_add);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        btn.setOnClickListener(this);
+
+        /*btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -56,13 +72,70 @@ public class SignUpActivity extends AppCompatActivity {
 
                 Toast.makeText(c, "Email: " + tempEmail + " " + "Password: " + tempPass, Toast.LENGTH_SHORT).show();
             }
+        });*/
+
+    }
+
+    private void registerEmail(){
+        Email = EmailRegTxt.getText().toString().trim();
+        Password = PassRegTxt.getText().toString().trim();
+
+        if(Email.isEmpty()){
+            EmailRegTxt.setError("Email is Required");
+            EmailRegTxt.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
+            EmailRegTxt.setError("Please enter a valid Email");
+            EmailRegTxt.requestFocus();
+            return;
+
+        }
+
+        if(Password.isEmpty()){
+            PassRegTxt.setError("Password is required");
+            PassRegTxt.requestFocus();
+            return;
+        }
+
+        if (Password.length()<6){
+            PassRegTxt.setError("Minimum length of Password should be 6 characters");
+            PassRegTxt.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "Sign Up Successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+                else
+                if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                    Toast.makeText(getApplicationContext(),"User is already registered", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.signupProceed:
+                registerEmail();
+                break;
 
 
-
-
-
-
-
+            case R.id.LogInTxt:
+                startActivity(new Intent(this, LogInActivity.class));
+                break;
+        }
     }
 }
